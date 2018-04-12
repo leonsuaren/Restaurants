df73dbcb602db1c391e138f596dd9bb1cd30f255
leonsuaren <leonsua@gmail.com>
2018-04-12 18:50:23 -0400

    All Apis are working correctly

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 var UserController = require('./../controllers/userController');
 var mongoose = require('mongoose');
-var User = require('./../models/user');
-var BodyParser = require('body-parser');
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/api', function(req, res, next) {
   var userController = new UserController(req, res, next);
   userController.getManyUsers();
 });
@@ -27,9 +23,16 @@ router.put('/:id', function(req, res, next){
   userController.updateOne();
 });
 
+router.put('/', function(req, res, next){
+  return res.status(405).json({error : "Invalid action"})
+});
+
 router.delete('/:id', function(req, res, next){
   var userController = new UserController(req, res, next);
   userController.deleteOne();
+});
+router.delete('/', function(req, res, next){
+  return res.status(405).json({error : "Invalid action"})
 });
 
 module.exports = router;
